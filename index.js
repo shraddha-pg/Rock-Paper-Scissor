d19ae3b3952011f4514a6ca43eb77748fc69507a
Shraddha Gaikwad <66875937+shraddha-pg@users.noreply.github.com>
2021-08-07 16:51:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const msg = document.getElementById("text");
-const button = document.getElementById("btn");
-const spin = ["Rock✊🏻", "Paper🖐🏻", "Scissor✌🏻"];
+const r = document.getElementById("rock");
+const p = document.getElementById("paper");
+const s = document.getElementById("Scissor");
+const values = ["Rock✊🏻", "Paper🖐🏻", "Scissor✌🏻"];
+const pc = Math.floor(Math.random() * 3);
+const user = document.getElementById("user");
+const comp = document.getElementById('pc');
+let userselection = "";
+let pcselection = values[pc];
+let result = document.getElementById('msg');
 
-button.addEventListener("click", function(){
-    let random = Math.floor(Math.random() * 3);
-    let dec = spin[random];
-    console.log(dec);
-    msg.textContent = dec;
+
+r.addEventListener("click", function () {
+  userselection = r.textContent;
+  user.textContent = `Your choice is: ${userselection}`;
+  comp.textContent = `Pc's choice is: ${pcselection}`;
+  result.textContent = win();
 });
+
+p.addEventListener("click", function () {
+  userselection = p.textContent;
+  user.textContent = `Your choice is: ${userselection}`;
+  comp.textContent = `Pc's choice is: ${pcselection}`;
+  result.textContent = win();
+});
+
+s.addEventListener("click", function () {
+  userselection = s.textContent;
+  user.textContent = `Your choice is: ${userselection}`;
+  comp.textContent = `Pc's choice is: ${pcselection}`;
+  result.textContent = win();
+});
+
+
+function win(){
+ if(userselection === pcselection ){
+ return "Oops! it's a tie😃";
+}else if (userselection === "Rock✊🏻" && pcselection  === "Scissor✌🏻" || userselection === "Paper🖐🏻" && pcselection  === "Rock✊🏻" || userselection === "Scissor✌🏻" && pcselection  === "Paper🖐🏻" ) {
+  return 'Hurrah ! You won🥳';
+}else{
+  return "Pc won the game😬";
+}
+}
